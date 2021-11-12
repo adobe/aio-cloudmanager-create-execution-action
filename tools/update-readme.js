c8075e8d77cd87abde00825398afabea36dc139e
Justin Edelson <jedelson@adobe.com>
2021-11-11 20:02:26 -0500

    chore(build): lint the tools file

--- tools/update-readme.js
+++ tools/update-readme.js
@@ -8,6 +8,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
+/* eslint-disable node/no-unpublished-require */
 
 const version = require('../package.json').version
 const replace = require('replace-in-file')
